5a7ea09a774963eb594affc03b4da02d3c4ec63c
yatsubashi <gakuyabushita@gmail.com>
2020-03-14 15:58:44 +0900

    Feature: Call Metals-API

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 const AWS = require("aws-sdk")
+const axios = require("axios")
 
 const region = "ap-northeast-1"
 const secretsManager = new AWS.SecretsManager({ region: region })
 
 const secretName = "metals-api"
 
+const endpoint = "https://metals-api.com/api/latest"
+const baseCurrency = "JPY"
+const symbols = "XAU"
+
 exports.lambdaHandler = async (event, context) => {
 	try {
 		const secretValue = await secretsManager.getSecretValue({ SecretId: secretName }).promise()
 		const { accessKey } = JSON.parse(secretValue.SecretString)
 
-		console.log(accessKey)
-		return accessKey
+		const url = `${endpoint}?access_key=${accessKey}&base=${baseCurrency}&symbols=${symbols}`
+		const result = await axios.get(url)
+
+		console.log(result)
 	} catch (err) {
 		console.log(err)
 		return err
